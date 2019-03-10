3c0f48dc09cba2dbdd81b6f2d8c2fc42c0273fcc
Kish533 <rkishore53352@gmail.com>
2019-03-10 14:38:30 +0530

    vamsi 1st commit

---
+++ src/main/java/packpack/FasakClass.java
@@ -0,0 +1,10 @@
+package packpack;
+
+public class FasakClass {
+
+public static void main(String[] args) {
+	System.out.println("kishore");
+}
+	
+	
+}
---
+++ src/test/java/eswaracharyulu/Buvanagiri.java
@@ -0,0 +1,12 @@
+package eswaracharyulu;
+
+import org.testng.annotations.Test;
+
+public class Buvanagiri {
+@Test
+public static void name() {
+	
+
+	System.out.println("buvanagiri eswaracharyulu");
+}
+}
